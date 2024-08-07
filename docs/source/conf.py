d0dada47cccd541c784db983d607a5e8ab933767
Kyle Xu <kyle.xu@recursionpharma.com>
2024-08-07 10:48:58 -0600

    change pat

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,13 @@ author = 'Graziella'
 release = '0.1'
 version = '0.1.0'
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath("../.."))
+
 # -- General configuration
 
 extensions = [
