66f5910d6c0edeff2c1206042b4df3e46b429de2
kxu03 <kxu@parktudor.org>
2024-08-07 21:55:20 -0600

    reauthor

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Syng-Bts'
+copyright = '2024, Xu'
+author = 'Xu'
 
 release = '0.1'
 version = '0.1.0'
